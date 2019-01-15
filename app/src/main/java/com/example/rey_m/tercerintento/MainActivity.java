9a595aea9360d3480df76525d0802d157b5e7fb4
kiikedls <jespinozadelossatnos@gmail.com>
2019-01-14 21:21:34 -0600

    agregue lo visto en clase de desarrollo

--- app/src/main/java/com/example/rey_m/tercerintento/MainActivity.java
+++ app/src/main/java/com/example/rey_m/tercerintento/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.rey_m.tercerintento;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.miboton);
+
+        /*Button f= findViewById(R.id.miboton);
+        f.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this,"nachos",Toast.LENGTH_LONG).show();
+            }
+        });*/
+        //esto es una nota
     }
 }
