3d3f59e71441e03f2aa2345f2d649c61a95c7023
kiikedls <jespinozadelossatnos@gmail.com>
2019-01-16 22:14:55 -0600

    agregue un textview para qe al final del contador dijera fin del juego. tambien egregue mas cases y nuevaslocaciones para el graviti del boton

--- app/src/main/java/com/example/rey_m/tercerintento/MainActivity.java
+++ app/src/main/java/com/example/rey_m/tercerintento/MainActivity.java
@@ -8,12 +8,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     Button btn1;
+    int cont=0;
+    TextView t;
+    TextView d;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,23 +25,37 @@ public class MainActivity extends AppCompatActivity {
         btn1=findViewById(R.id.miboton);
 
         btn1=findViewById(R.id.btn1);
+        t=findViewById(R.id.texcont);
+        t.setText("Clicks");
+        d=findViewById(R.id.texdone);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cont++;
+                t.setText("clicks: "+String.valueOf(cont));
+            }
+        });
 
         new CountDownTimer(10000, 500) {
             @Override
             public void onTick(long l) {
 
-
                 FrameLayout.LayoutParams lp=new FrameLayout.LayoutParams(-2,-2);
                 /*lp.gravity=Gravity.TOP;
                 btn1.setLayoutParams(lp);*/
                 Random r=new Random();
-                int n = r.nextInt(3) + 1;
+                int n = r.nextInt(5) + 1;
                 switch (n){
-                    case 1:lp.gravity=Gravity.BOTTOM;
+                    case 1:lp.gravity=Gravity.TOP|Gravity.LEFT;
                         break;
-                    case 2:lp.gravity=Gravity.LEFT;
+                    case 2:lp.gravity=Gravity.TOP|Gravity.RIGHT;
                     break;
-                    case 3:lp.gravity=Gravity.RIGHT;
+                    case 3:lp.gravity=Gravity.BOTTOM|Gravity.RIGHT;
+                    break;
+                    case 4:lp.gravity=Gravity.BOTTOM|Gravity.LEFT;
+                    break;
+                    case 5:lp.gravity=Gravity.CENTER;
                     break;
                 }
                 btn1.setLayoutParams(lp);
@@ -45,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-
+                d.setText("juego terminado");
             }
         }.start();
 
